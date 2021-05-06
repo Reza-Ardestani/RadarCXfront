f353f0b75631b258546eb90cc54d82cc975b9e88
mmafshari77 <mahdiafshari@aut.ac.ir>
2021-05-06 16:51:48 +0430

    threading uncommented

--- radarcx/radarcxapp/views.py
+++ radarcx/radarcxapp/views.py
@@ -35,23 +35,37 @@ class new_cond(View):
 import time
 import threading
 
+def notifgeneral():
+    pass
+
+def notifSpecitic(userToken):
+    #Notify user
+    pass
+
 def conditionsChecker():
     #here we check whether any of conditions has been triggerd or #
-    ##print("i'm in conditions cheker function")
-    # call notification func if it's necessary
+
+    # call "notifSpecitic" func if it's necessary
     pass
 
 def fetchData_and_check():
-    print(user.objects.values())
-    #while(True):
-    #    #startTime = perf_counter()
-    #    ##print("here I receive data of all coins and store them in DB")
-    #    time.sleep(3)
-    #    conditionsChecker()#Synchronizicly we runn this fun & wait till it ends
-    #    time.sleep(1)
+    #print(user.objects.values())
+    while(True):
+        startOfLoopTime = perf_counter()
+        print("here I receive data of all coins and store them in DB")
+        ##
+        # I will provide you code of API call for getting data of coins
+        ##
+
+        conditionsChecker()#Synchronizicly we runn this fun & wait till it ends
+
+        endOfLoopTime = perf_counter()
+        if(endOfLoopTime-startOfLoopTime<60):
+            sleep(round(endOfLoopTime-startOfLoopTime,0))
+
 
 # doing work
-#coinsData_thread = threading.Thread(target= fetchData_and_check )
-#coinsData_thread.start()
+coinsData_thread = threading.Thread(target= fetchData_and_check )
+coinsData_thread.start()
 # print("last line of main process")
 ''' End of threading '''
