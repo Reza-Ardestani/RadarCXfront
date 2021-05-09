c5e2283f64d5369f37d662c9f7695617d41c2507
MohammadMahdi Afshari <62270016+mmafshari77@users.noreply.github.com>
2021-05-09 21:07:06 +0430

    update settings file and requirements file to include heroku postgres sql

--- radarcx/settings.py
+++ radarcx/settings.py
@@ -97,6 +97,10 @@ DATABASES = {
     }
 }
 
+# comment this while in your local host 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
