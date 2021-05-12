0654455fa3e4ae086ec7556fdfa0e92527e3e626
Matthew Anderson <76392391+MateAnderson@users.noreply.github.com>
2021-05-12 19:15:29 +0430

    implementing NotifSpecific func

--- radarcxapp/bgthread.py
+++ radarcxapp/bgthread.py
@@ -15,7 +15,7 @@ def get_onesignal_accounts():
     from najva_api_client.najva import Najva
     client = Najva()
     client.apikey = "9e48e9f0-41e1-4f0a-a3d0-dd34b8313f03"
-    client.token = "4fa9220c03c0483ccd5dbced0b83a78c9e242e3f"
+    client.token = "6e83d4164baf569f345ab556b01347b1178776c5"
 
     # getting all connected clients to our website
     accounts_str = client.get_onesignal_accounts()
@@ -25,9 +25,26 @@ def notifgeneral():
     pass
 
 
-def notifSpecific(userToken):
-    # Notify user
-    pass
+def notifSpecific(userToken , titleText , bodyText ):
+    #make sure userToken is str and in other forms
+    from najva_api_client.najva import Najva
+    # Najve requirements for the Notify_specific_user:
+    #send_to_users(self,title, body, url, icon,subscriber_tokens,
+    #              onclick='open-link', image=None,
+    #              content=None,json=None,sent_time=None):
+    client = Najva()
+    client.apikey = "9e48e9f0-41e1-4f0a-a3d0-dd34b8313f03"
+    client.token = "6e83d4164baf569f345ab556b01347b1178776c5"
+
+    notifSpecific_response = client.send_to_users(title=titleText,
+    body= bodyText,
+    url="https://radarcx.herokuapp.com/",
+    icon="https://png.pngtree.com/element_our/md/20180515/md_5afb099d307d3.jpg",
+    image="https://png.pngtree.com/element_our/md/20180515/md_5afb099d307d3.jpg",
+    onclick="open-link",
+    subscriber_tokens=[userToken])
+
+    return notifSpecific_response
 
 
 def conditionsChecker():
