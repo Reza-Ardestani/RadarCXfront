e03e35d8703cee6f06f99254fcdf89a2881decf0
Ahmad1378 <61465556+Ahmad1378@users.noreply.github.com>
2021-05-12 11:32:18 +0430

    set urls for register view

--- radarcx/urls.py
+++ radarcx/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('', include('radarcxapp.urls')),
+    path('register/', user_views.register, name='register'),
+    path('', include('radarcxapp.urls')),
 ]
