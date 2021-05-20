77cddd8dfebd28592be31728b300cf412cbded78
Matthew Anderson <76392391+MateAnderson@users.noreply.github.com>
2021-05-20 23:18:56 +0430

    debuging on Coins.realtime_price

--- radarcxapp/bgthread.py
+++ radarcxapp/bgthread.py
@@ -12,23 +12,28 @@ def conditionsChecker():
     ''' note: we have not implemented 'equal' and 'moving_average','volume' yet. '''
     conditions = Condition.objects.all()
     for condition in conditions:
-        coin_quantity = Coin.objects.filter(name=condition.coin).last()
-        user  = User.objects.filter(id=condition.creator_id).first()
-        usertokens = user.usertoken_set.all()
-        print(usertokens)
+        coin_quantity = Coin.objects.filter(name=condition.coin).last().realtime_price
+        if condition.smaller_or_greater == "g" and coin_quantity >= condition.quantity :
+            username = User.objects.filter(id=condition.creator_id).first()
+            coinName= condition.coin
+            Bodytext = "Dear" + username + "your condition on"  + coinName +"at price"+ str(condition.quantity) + "has been triggered "
+            user  = User.objects.filter(id=condition.creator_id).first()
+            usertokens = user.usertoken_set.all()
+            one_user_tokens = [ record.token for record in usertokens ]
+            NajveResponse = send_to_users(body= Bodytext,
+            subscriber_tokens= one_user_tokens,
+            sent_time=UTC_to_IR_TimeZone())
 
-        # if condition.smaller_or_greater == "g" and coin_quantity >= condition.quantity :
-        #     #print(f"The {condition.type} of {condition.coin} is greater than {condition.quantity} ")
-        #     username = User.objects.filter(id=condition.creator_id).first()
-        #     coinName= condition.coin
-        #     Bodytext = "Dear" + username + "your condition on"  + coinName +"at price"+condition.quantity + "has been triggered "
-        #     user_token = ""
-        #     NajveResponse = send_to_users(body= Bodytext,
-        #     subscriber_tokens=[user_token],
-        #     sent_time=UTC_to_IR_TimeZone())
-
-        # if condition.smaller_or_greater == "s" and coin_quantity <= condition.quantity :
-        #     print(f"The {condition.type} of {condition.coin} is smaller than {condition.quantity} ")
+        if condition.smaller_or_greater == "s" and coin_quantity <= condition.quantity :
+            username = User.objects.filter(id=condition.creator_id).first()
+            coinName= condition.coin
+            Bodytext = "Dear" + username + "your condition on"  + coinName +"at price"+ str(condition.quantity) + "has been triggered "
+            user  = User.objects.filter(id=condition.creator_id).first()
+            usertokens = user.usertoken_set.all()
+            one_user_tokens = [ record.token for record in usertokens ]
+            NajveResponse = send_to_users(body= Bodytext,
+            subscriber_tokens= one_user_tokens,
+            sent_time=UTC_to_IR_TimeZone())
 
 
 
