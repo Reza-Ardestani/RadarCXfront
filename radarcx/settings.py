6294fe61565e7ec1bbccd05526afad5b1b0b5dfd
Ahmad1378 <61465556+Ahmad1378@users.noreply.github.com>
2021-05-21 21:52:00 +0430

    some changes in profile page

--- radarcx/settings.py
+++ radarcx/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "staticfiles"
 # ]
--- radarcx/urls.py
+++ radarcx/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('users.urls')),
     path('', include('radarcxapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
