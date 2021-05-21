6b160a10f57f27d9779d703c4032b3d0c5b8a4a7
MohammadMahdi Afshari <62270016+mmafshari77@users.noreply.github.com>
2021-05-21 17:54:47 +0430

    uncomment price checking

--- radarcxapp/bgthread.py
+++ radarcxapp/bgthread.py
@@ -38,38 +38,23 @@ def conditionsChecker():
 
 
 def fetchData_and_check():
-    conditionsChecker()
-    '''
-    while(True):
-        # startOfLoopTime = perf_counter()
-        # print("here I receive data of all coins and store them in DB")
-        url = 'https://min-api.cryptocompare.com/data/price'
+    # while(True): 
+    # print("here I receive data of all coins and store them in DB")
+    url = 'https://min-api.cryptocompare.com/data/price'
 
-        parameters = {'fsym': "BTC",
-                    'tsyms': "USD"}
-        exchange = ''
+    parameters = {'fsym': "BTC",
+                'tsyms': "USD"}
+    exchange = ''
 
-        if exchange:
-            print('exchange: ', exchange)
-            parameters['e'] = exchange
+    if exchange:
+        print('exchange: ', exchange)
+        parameters['e'] = exchange
 
-        # response comes as json
-        response = requests.get(url, params=parameters)
-        data = response.json()
+    # response comes as json
+    response = requests.get(url, params=parameters)
+    data = response.json()
 
-        cleanup = Coin.objects.all()
-        cleanup.delete()
-        cleanup = Condition.objects.all()
-        cleanup.delete()
-
-        c = Coin(name="BTC", realtime_price=123, moving_average=123, volume=213)
-        c.save()
-        c = Coin.objects.get(name=parameters["fsym"])
-        c.realtime_price=data[parameters["tsyms"]]
-        c.save()
-
-        conditionsChecker()  # Synchronizicly we runn this fun & wait till it ends
-        '''
-        # endOfLoopTime = perf_counter()
-        # if(endOfLoopTime-startOfLoopTime < 60):
-        #     sleep(round(endOfLoopTime-startOfLoopTime, 0))
+    c = Coin.objects.filter(name=parameters["fsym"]).first()
+    c.realtime_price=data[parameters["tsyms"]]
+    c.save()
+    conditionsChecker()  # Synchronizicly we runn this fun & wait till it ends
