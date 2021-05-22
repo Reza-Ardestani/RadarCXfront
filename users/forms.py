fc6f1c5c4923f396278a2c6a00973d08fbf4fc76
Ahmad1378 <61465556+Ahmad1378@users.noreply.github.com>
2021-05-22 19:28:02 +0430

    now user can update his/her profile

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
