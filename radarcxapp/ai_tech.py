b60bb9a2c646ad2f54366226054a34d4f90ba358
Matthew Anderson <76392391+MateAnderson@users.noreply.github.com>
2021-05-23 17:02:48 +0430

    " handling path to the googleDrive csv file"

--- radarcxapp/ai_tech.py
+++ radarcxapp/ai_tech.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import requests
 
+#Due to Heroku restrictions, we need to store date on other clouds and pass its link
 url = "https://drive.google.com/file/d/1WZzdZBT4QSrLKWHmA1KRUVm-AKE-LOGK/view?usp=sharing"
-df = pd.read_csv(url)
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df = pd.read_csv(path)
 print(df.head())
